bdc1a8a433225a36e501c04c15c2b008ab7dc54a
Pete Looney <petelooney81@gmail.com>
2020-09-17 15:37:40 -0500

    working

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ document.addEventListener('DOMContentLoaded', function(){
             console.log(currentMovie);
             return `
             <div class="movie mr-1 mt-1 col-3">
-            <div class="card">
+            <div class="card h-85">
               <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
-              <div class="card-body d-flex pb-0">
+              <div class="card-body d-flex justify-content-around p-0 mt-1">
                 <h7 class="card-title" id="movie-title" style="font-size: .75rem;">${currentMovie.Title}</h7>
-                <p class='btn btn-secondary' id='movie-year' style="height: 2rem; font-size: .75rem;">${currentMovie.Year}</p>
+                <p class='btn btn-secondary' id='movie-year' style="font-size: .75rem; padding: 0; margin: 0; height: 1.25rem;">${currentMovie.Year}</p>
               </div>
-              <div class="card-body d-flex justify-content-center" style="padding-top: 0;">
-                <button class="btn btn-primary">Add</button>
+              <div class="card-body d-flex justify-content-center" style="padding: 0; margin: 0;">
+                <button class="btn btn-primary m-1 p-1" style="font-size: .75rem;">Add</button>
               </div>
             </div>
           </div>
