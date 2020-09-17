ab472bef84f423b508fa0f33d09d2bb6d319eacd
Pete Looney <petelooney81@gmail.com>
2020-09-17 16:00:46 -0500

    attempting part 2

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 
 
 document.addEventListener('DOMContentLoaded', function(){
+    function saveToWatchList(imbdID){
+        console.log(imbdID);
+    }
+    
     function renderMovies(movieArray){
         const movieHtmlArray = movieArray.map((currentMovie) => {
 
             console.log(currentMovie);
             return `
             <div class="movie mr-1 mt-1 col-3">
-            <div class="card h-85">
+            <div class="card" style="height: 18rem">
               <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
               <div class="card-body d-flex justify-content-around p-0 mt-1">
                 <h7 class="card-title" id="movie-title" style="font-size: .75rem;">${currentMovie.Title}</h7>
                 <p class='btn btn-secondary' id='movie-year' style="font-size: .75rem; padding: 0; margin: 0; height: 1.25rem;">${currentMovie.Year}</p>
               </div>
               <div class="card-body d-flex justify-content-center" style="padding: 0; margin: 0;">
-                <button class="btn btn-primary m-1 p-1" style="font-size: .75rem;">Add</button>
+                <button class="btn btn-primary m-1 p-1" id="addMovieBtn" style="font-size: .75rem; height: 2rem;" onclick="saveToWatchList('${currentMovie.imbdID}')">Add</button>
               </div>
             </div>
           </div>
@@ -22,10 +26,12 @@ document.addEventListener('DOMContentLoaded', function(){
         });
         return movieHtmlArray.join('');
     }
+
+
+
     document.addEventListener('click', (e)=>{
         e.preventDefault();
         if(e.target.id === 'search'){
-            console.log('Hello');
             const movieContainer = document.querySelector('.movie-container');
             movieContainer.innerHTML = renderMovies(movieData);
         }
